f9edea6a0ba0a928af14fc8f1d6a6b42eb082f6c
Brandon Villa <bornofos@gmail.com>
2019-03-18 23:01:58 -0600

    override Document component

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Main, NextScript, NextDocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: NextDocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+export default MyDocument
