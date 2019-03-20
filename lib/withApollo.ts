1901ff71e78e016ede075acbde5e24b9769d23cb
Brandon Villa <bornofos@gmail.com>
2019-03-19 19:16:55 -0600

    webpack config for inject env variables
    dotenv & webpack-dotenv were installed for this process

--- lib/withApollo.ts
+++ lib/withApollo.ts
@@ -4,7 +4,6 @@ import ApolloClient  from 'apollo-boost'
 // TODO fix any type
 function createClient(client: any) {
   const { headers } = client
-  console.log(process.env.GRAPHQL_URL)
   return new ApolloClient({
     uri: process.env.GRAPHQL_URL,
     // Not working due incompatible Types
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
-module.exports = withTypescript(withSass())
+require('dotenv').config()
+
+
+module.exports = withTypescript(withSass({
+  webpack: config => {
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
+
+    return config
+  }
+}))
