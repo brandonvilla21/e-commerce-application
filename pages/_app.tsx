80d656745e5bf2dd9f1b107f2dac20410b6625db
Brandon Villa <bornofos@gmail.com>
2019-03-21 19:04:21 -0600

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import '../lib/bootstrap';
+// --- ^^^ must be at the top ^^^ ---
+
 import React from 'react'
 import App, { Container, NextAppContext } from 'next/app'
 import { ApolloProvider } from 'react-apollo'
@@ -5,11 +8,28 @@ import { WithApolloClient } from 'react-apollo/withApollo'
 import withApollo from '../lib/withApollo'
 import Page from '../components/Page'
 import { ApolloClient } from 'apollo-boost'
+import getPageContext from '../lib/getPageContext';
+import { StylesProvider, ThemeProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
 
 interface MyAppProps {
   apollo: ApolloClient<any>
 }
 class MyApp extends App<WithApolloClient<MyAppProps>> {
+  constructor() {
+    // @ts-ignore
+    super()
+    this.pageContext = getPageContext()
+  }
+  
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   static async getInitialProps(context: NextAppContext) {
     const { Component, ctx } = context
     let pageProps = {}
@@ -28,7 +48,19 @@ class MyApp extends App<WithApolloClient<MyAppProps>> {
       <Container>
         <ApolloProvider client={apollo}>
           <Page>
-            <Component {...pageProps} />
+            {/* Wrap every page in Styles and Theme providers */}
+            <StylesProvider
+              generateClassName={this.pageContext.generateClassName}
+              sheetsRegistry={this.pageContext.sheetsRegistry}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              {/* ThemeProvider makes the theme available down the React tree thanks to React context. */}
+              <ThemeProvider theme={this.pageContext.theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Component pageContext={this.pageContext} {...pageProps} />
+              </ThemeProvider>
+            </StylesProvider>
           </Page>
         </ApolloProvider>
       </Container>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,29 @@
-import Document, { Head, Main, NextScript, NextDocumentContext } from 'next/document'
+import Document, { Head, Main, NextScript, NextDocumentContext, AnyPageProps, PageProps } from 'next/document'
+import { MuiThemeProviderProps } from '@material-ui/core/styles/MuiThemeProvider'
+import { PageContext } from '../lib/getPageContext'
+import React, { ComponentType } from 'react'
+import flush from 'styled-jsx/server'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: NextDocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+class MyDocument extends Document<{ muiThemeProvider: MuiThemeProviderProps }> {
+
+  // static async getInitialProps(ctx: NextDocumentContext) {
+
+  // }
 
   render() {
+    const { muiThemeProvider } = this.props
+    const theme = typeof muiThemeProvider.theme === 'function' ? muiThemeProvider.theme(null) : muiThemeProvider.theme
+    const themeColor = theme.palette.primary.main
+
     return (
-      <html>
+      <html lang="en" dir="ltr">
         <Head />
+        <meta charSet="utf-8" />
+        {/* Use minimum-scale=1 to enable GPU rasterization */}
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+        {/* PWA primary color */}
+        <meta name="theme-color" content={themeColor} />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
         <body>
           <Main />
           <NextScript />
@@ -17,6 +31,43 @@ class MyDocument extends Document {
       </html>
     )
   }
+}
+interface PagePropsWithMuiContext extends AnyPageProps {
+  pageContext: PageContext
+}
+
+// TODO: see differnce between static getInitialProps to see if is better than this approach
+MyDocument.getInitialProps = ctx => {
+  let pageContext: PageContext | undefined
+
+  const page = ctx.renderPage((Component: ComponentType<PagePropsWithMuiContext>) => {
+    const WrappedComponent: ComponentType<{ pageContext: PageContext } & PageProps> = props => {
+      pageContext = props.pageContext
+      return <Component {...props} />
+    }
+
+    return WrappedComponent
+  })
+
+  let css
+  // It might be undefined, e.g. after an error.
+  if (pageContext) {
+    css = (pageContext as PageContext).sheetsRegistry.toString()
+  }
+
+  // const initialProps = await Document.getInitialProps(ctx)
+  // return { ...initialProps }
+  return {
+    ...page,
+    pageContext,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: (
+      <React.Fragment>
+        <style id="jss-server-side" dangerouslySetInnerHTML={{ __html: css as string }} />
+        {flush() || null}
+      </React.Fragment>
+    ),
+  };
 }
 
 export default MyDocument
