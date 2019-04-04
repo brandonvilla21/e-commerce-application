7726c174c685324e2adc8ee66d1a794a2ed94c4c
Brandon Villa <bornofos@gmail.com>
2019-04-03 22:28:18 -0600

    add LoginForm component in order to perform request to GraphQL server from this component

---
+++ components/Login/LoginForm/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo';
+import useInput from '../../../hooks/useInput';
+import { TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '700px',
+    padding: '0 .5em',
+  },
+  textField: {
+    margin: '.5em 0',
+  },
+  button: {
+    marginTop: '.5em',
+  }
+});
+
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token,
+      user {
+        id,
+        name,
+        email
+      }
+    }
+  }
+`;
+
+const LoginForm = ({ onSubmitted }) => {
+  const email = useInput('email')
+  const password = useInput('password')
+  const classes = useStyles()
+
+  const handleSubmit = async (event: any, login: any) => {
+    try {
+      event.preventDefault();
+      const response = await login()
+      onSubmitted(response, null)
+    } catch (err) {
+      onSubmitted(null, err)
+    }
+  }
+
+  return (
+    <Mutation
+      mutation={LOGIN_MUTATION}
+      variables={{ email: email.value, password: password.value }}
+    >
+      {(login, { error, data, loading }) => (
+        <form className={classes.form} method="post" onSubmit={e => handleSubmit(e, login)}>
+          {error && <code>{JSON.stringify(error)}</code>}
+          {data && <code>{JSON.stringify(data)}</code>}
+          {loading && <code>loading...</code>}
+
+          <TextField {...email} className={classes.textField} />
+          <TextField type="password" {...password} className={classes.textField} />
+          <Button type="submit" variant="contained" color="primary" className={classes.button}>
+            Login
+          </Button>
+        </form>
+      )}
+    </Mutation>
+  )
+}
+
+export default LoginForm
