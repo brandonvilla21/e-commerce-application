d9834b8a605b4015543214b1101f7d12d71ac6b3
marcos8896 <elyomarcos@gmail.com>
2019-05-20 21:38:04 -0500

    Add main validation for the CreateItem component - #14

--- components/Item/CreateItem/index.tsx
+++ components/Item/CreateItem/index.tsx
@@ -3,6 +3,8 @@ import gql from 'graphql-tag';
 import Router from 'next/router';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 import { makeStyles } from '@material-ui/styles';
 import { MutationFn } from 'react-apollo-hooks';
 import {
@@ -12,15 +14,34 @@ import {
 } from '../../generated/apollo-components';
 import useForm from '../../../hooks/useForm';
 import SelectImage from './components/selectImage';
-import Snackbar from '@material-ui/core/Snackbar';
 import SnackbarContentWrapper from '../../shared/SnackbarContentWrapper';
+
+const blueGrey100 = blueGrey['100'];
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
     margin: '25px',
+    border: `1px solid ${blueGrey100}`,
+    padding: '10px',
   },
+  inputsContainer: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexFlow: 'row wrap',
+  },
+  field: {
+    width: '400px',
+  },
+  button: {
+    margin: theme.spacing.unit,
+    maxWidth: '300px',
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  }
 }));
 
 const CREATE_ITEM_MUTATION = gql`
@@ -62,6 +83,20 @@ function submitItem(
   // createItem({variables: item});
 };
 
+function isFormValid(
+  item: CreateItemMutationVariables,
+  imageData: {
+    selectedImage: null,
+    srcImage: string,
+    isImageToLarge: boolean,
+  },
+) {
+  return item.description === '' ||
+         item.title === '' ||
+         !imageData.selectedImage ||
+         imageData.isImageToLarge;
+}
+
 function CreateItem() {
   const { values, handleChange } = useForm(initialState);
   const [imageData, setImageData] = useState({ selectedImage: null, srcImage: '', isImageToLarge: false });
@@ -71,42 +106,55 @@ function CreateItem() {
       <>
         <form className={classes.root} onSubmit={e => submitItem(e, createItem, values)}>
 
-        <SelectImage setImageData={setImageData} imageData={imageData}/>
+          <SelectImage setImageData={setImageData} imageData={imageData}/>
 
-        <TextField
-          id="title"
-          name="title"
-          label="Título"
-          placeholder="Ingrese el título de su artículo"
-          margin="normal"
-          value={values.title}
-          onChange={handleChange}
-          type="text"
-        />
-        <TextField
-          id="description"
-          name="description"
-          label="Descripción"
-          placeholder="Ingrese la descripción de su artículo"
-          margin="normal"
-          value={values.description}
-          onChange={handleChange}
-          type="text"
-        />
-        <TextField
-          id="price"
-          name="price"
-          label="Precio"
-          placeholder="Ingrese el precio de su artículo"
-          margin="normal"
-          value={values.price}
-          onChange={handleChange}
-          type="number"
-          InputProps={{ inputProps: { min: 0 } }}
-        />
-        <Button variant="contained" color="primary" type="submit">
-          Registrar artículo
-        </Button>
+          <div className={classes.inputsContainer}>
+            <TextField
+              className={classes.field}
+              id="title"
+              name="title"
+              label="Título"
+              placeholder="Ingrese el título de su artículo"
+              margin="normal"
+              value={values.title}
+              onChange={handleChange}
+              type="text"
+              variant="outlined"
+            />
+            <TextField
+              className={classes.field}
+              id="price"
+              name="price"
+              label="Precio"
+              placeholder="Ingrese el precio de su artículo"
+              margin="normal"
+              value={values.price}
+              onChange={handleChange}
+              type="number"
+              InputProps={{ inputProps: { min: 0 } }}
+              variant="outlined"
+            />
+          </div>
+
+          <TextField
+            id="description"
+            name="description"
+            label="Descripción"
+            placeholder="Ingrese la descripción de su artículo"
+            margin="normal"
+            value={values.description}
+            onChange={handleChange}
+            type="text"
+            multiline
+            rows="4"
+            variant="outlined"
+          />
+
+          <div className={classes.buttonContainer}>
+            <Button disabled={isFormValid(values, imageData)} className={classes.button} variant="contained" color="primary" type="submit">
+              Registrar artículo
+            </Button>
+          </div>
         </form>
 
         <Snackbar
