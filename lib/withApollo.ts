5452c58032b81acdba14ca1118a81d7968c1e12e
marcos8896 <elyomarcos@gmail.com>
2019-05-21 21:01:49 -0500

    Add and configure apollo-upload-client - #14

--- lib/withApollo.ts
+++ lib/withApollo.ts
@@ -1,23 +1,35 @@
-import withApollo from 'next-with-apollo'
-import ApolloClient, { Operation }  from 'apollo-boost'
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { createUploadLink } from 'apollo-upload-client';
+import withApollo from 'next-with-apollo';
 
 // TODO fix type
 function createClient(client: any) {
-  const { headers } = client
+  // Modify headers object if we need to send extra header in the future.
+  let { headers } = client
   return new ApolloClient({
-    uri: process.env.GRAPHQL_URL,
-    request: (operation: Operation) => {
-      return new Promise((resolve) => {
-        operation.setContext({
-          fetchOptions: {
-            credentials: 'include'
-          },
-          headers,
-        })
-        resolve()
-      })
-    }
-  })
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors)
+          graphQLErrors.map(({ message, locations, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
+          );
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+      }),
+      createUploadLink({
+        uri: process.env.GRAPHQL_URL,
+        fetchOptions: {
+          credentials: 'include',
+        },
+        headers,
+      }),
+    ]),
+    cache: new InMemoryCache()
+  });
 }
 
-export default withApollo(createClient)
+export default withApollo(createClient);
